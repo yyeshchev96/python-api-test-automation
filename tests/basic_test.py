b0508c1eaddc246dd9551ab536fa72ab587fc945
Yevhen Yeshchev <yyeshchev@gmail.com>
2023-03-26 21:51:06 +0300

    added simple test for httpx

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,3 +1,4 @@
+import httpx
 import pytest
 from assertpy import assert_that
 
@@ -9,3 +10,8 @@ def test_passing_scenario():
 @pytest.mark.xfail
 def test_failing_scenario():
     assert_that(False).is_true()
+
+
+def test_httpx_get_google_search_python_returns_200():
+    resp = httpx.get("https://httpbin.org/json")
+    assert_that(resp.status_code).is_equal_to(200)
