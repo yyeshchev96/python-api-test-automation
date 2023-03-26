46d565cb4ce8abcf09db0e703f4857473c79b89c
Yevhen Yeshchev <38920620+yyeshchev96@users.noreply.github.com>
2023-03-26 21:54:14 +0300

    Added github action to run the tests (#1)
    * added github action to run the tests
    
    * added simple test for httpx

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,3 +1,5 @@
+import httpx
+import pytest
 from assertpy import assert_that
 
 
@@ -5,5 +7,11 @@ def test_passing_scenario():
     assert_that(True).is_true()
 
 
+@pytest.mark.xfail
 def test_failing_scenario():
     assert_that(False).is_true()
+
+
+def test_httpx_get_google_search_python_returns_200():
+    resp = httpx.get("https://httpbin.org/json")
+    assert_that(resp.status_code).is_equal_to(200)
